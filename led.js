e83908a11f1cb4a4d50fc99691feac6eec49bb24
alexdobjanschi <alex.dobjanschi@gmail.com>
2017-12-02 20:20:54 +0100

    gpio nodejs wiring

--- led.js
+++ led.js
@@ -1,25 +1,56 @@
 const awsIot = require('aws-iot-device-sdk')
+const fs = require('fs')
+const gpio = require('rpi-gpio')
 
-const thing = 'RedLED'
+const usageAndExit() => {
+  console.log(`Usage`)
+  console.log(`\tnode led.js --config=<configuration file>`)
+  process.exit(1)
+}
 
-const shadow = awsIot.thingShadow({
-    caPath: process.env.caPath,
-   keyPath: process.env.keyPath,
-  certPath: process.env.certPath,
-  clientId: process.env.clientId,
-      host: process.env.host
+if (process.argv.length < 3) {
+  usageAndExit()
+}
+
+const args = require('minimist')(process.argv.slice(2))
+if (!args.config) {
+  console.log('Missing --config argument')
+  usageAndExit()
+}
+if (!args.config.thingName) {
+  console.log('Missing thingName from config file')
+  usageAndExit()
+}
+
+if (!args.config.pin) {
+  console.log('Missing output pin from config file')
+  usageAndExit()
+}
+
+const pin = parseInt(args.config.pin)
+
+gpio.setup(pin, gpio.DIR_OUT, (err) => {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })
+
+const thing = args.thingName
+const shadow = awsIot.thingShadow(JSON.parse(fs.readFileSync(args.config, 'utf8')))
 
 let clientTokenUpdate
 
 shadow.on('connect', function() {
-    thingShadows.register(thing, {}, function() {
+    shadow.register(thing, {}, function() {
       const state = {
-        state: {
-          desired: "off"
+        "state": {
+          "desired": {
+            "toggle": "off"
+          }
         }
       }
-      clientTokenUpdate = shadow.update(thing, state);
+      clientTokenUpdate = shadow.update(thing, state)
       if (clientTokenUpdate === null) {
         console.log('update shadow failed, operation still in progress')
       }
@@ -27,11 +58,17 @@ shadow.on('connect', function() {
 })
 
 shadow.on('status', (thingName, stat, clientToken, stateObject) => {
-  console.log(`received ${stat} on ${thingName}: ${JSON.stringify(stateObject)}`)
+  console.log(`received status '${stat}' on ${thingName}: ${JSON.stringify(stateObject)}`)
 })
 
 shadow.on('delta', (thingName, stateObject) => {
   console.log(`received delta on ${thingName}: ${JSON.stringify(stateObject)}`)
+  if (stateObject.state && stateObject.state.toggle) {
+    gpio.write(pin, stateObject.state.toggle === "on", (err) => {
+      if (err) throw err
+      console.log('Written to pin')
+    })
+  }
 })
 
 shadow.on('timeout', (thingName, clientToken) => {
